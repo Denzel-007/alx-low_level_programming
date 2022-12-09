53abcd7015777e5912684f018c926978a242c960
Denzel-007 <akinlaluredaniel@gmail.com>
2022-12-09 06:47:54 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,9 +11,8 @@ int main(void)
 {
 	int n;
 
-
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand () - RAND_MAX / 2;
 
 	if (n > 0)
 	{
@@ -27,5 +26,6 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	return (0);
+
+	return (0)
 }
