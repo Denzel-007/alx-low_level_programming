265b8cbc3c7725d4db64ce9b9c45a769344dedff
Denzel-007 <akinlaluredaniel@gmail.com>
2022-12-15 14:28:15 -0800

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,21 +12,21 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		m = n * -l;
 	}
 	else
 	{
 		m = n;
 	}
 	d = m;
-	count = 1;
+	count = l;
 
 	while (d > 9)
 	{
 		d /= 10;
 		count *= 10;
 	}
-	for (; count >= 1; count /= 10)
+	for (; count >= l; count /= 10)
 	{
 		_putchar(((m / count) % 10) + 48);
 	}
