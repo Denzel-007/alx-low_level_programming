490c281df6dac106ecdb52bf5185a9b621a57f36
Denzel-007 <akinlaluredaniel@gmail.com>
2022-12-22 07:21:41 -0800

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,32 +11,32 @@ void print_buffer(char *b, int size)
 
 	if (size < 0)
 	{
-		_putchar('\n');
+		printf('\n');
 		return;
 	}
 	while (i < size)
 	{
 		if (i % 10 == 0)
-			_putchar("%08x: ", i);
+			printf("%08x: ", i);
 		for (j = i; j < i + 9; j += 2)
 		{
 			if ((j < size) && ((j + 1) < size))
-				_putchar("%02x%02x: ", b[j], b[j + 1]);
+				printf("%02x%02x: ", b[j], b[j + 1]);
 			else
 			{
 				while (++j <= i + 10)
-					_putchar(" ");
-				_putchar(" ");
+					printf(" ");
+				printf(" ");
 			}
 		}
 		for (j = i; j < i + 9 && j < size; j++)
 		{
 			if (b[j] >= 32 && b[j] <= 126)
-				_putchar("%c", b[j]);
+				printf("%c", b[j]);
 			else
-				_putchar(".");
+				printf(".");
 		}
-		_putchar('\n');
+		printf('\n');
 		i += 10;
 	}
 }
