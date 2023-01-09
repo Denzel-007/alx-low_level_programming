8a23a066e962546ceb93f6007746523cff73cf70
Denzel-007 <akinlaluredaniel@gmail.com>
2023-01-09 12:50:28 -0800

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,11 +20,13 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(sizeof(char) * size);
 
+	/*check if malloc was successful */
+
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
-	return (arr)
+	return (arr);
 }
