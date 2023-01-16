1af0b581ab145ad7b2bcf95473695b5dc150ad54
Denzel-007 <akinlaluredaniel@gmail.com>
2023-01-16 09:48:18 -0800

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,6 +5,7 @@
  * @d: pointer to structure.
  * @name: first member.
  * @age: second member.
+ * @owner: third member.
  * Return: void.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -14,5 +15,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 
 	d->name = name;
 	d->age = age;
-	d-> = owner;
+	d->owner = owner;
 }
