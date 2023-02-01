a3e9181fe42dda4dc38faa87d1007c3613cf57d3
Denzel-007 <akinlaluredaniel@gmail.com>
2023-02-01 07:48:08 -0800

    Task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,6 +2,7 @@
 
 /**
  * sum_listint - calculates the sum of all the data in a listint_t list
+ * @head: first node in the linked list
  * Return: resulting sum
  */
 int sum_listint(listint_t *head)
